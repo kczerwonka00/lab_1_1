9b5d0606d0c3198853e3a47fb8f0d061464a87aa
kczerwonka00 <101180432+kczerwonka00@users.noreply.github.com>
2022-03-22 09:54:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,5 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("H"+"\n"+"e"+"\n"+"l"+"\n"+"l"+"\n"+"o"+"\n"+"\n"+"w"+"\n"+"o"+"\n"+"r"+"\n"+"l"+"\n"+"d"+"\n"+"!");
+  }
+}
